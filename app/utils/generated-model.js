92621f7001ad7a9cba91b6df7edc452493d72b9f
Nathan Palmer <nathan@nathanpalmer.com>
2015-02-11 11:02:12 -0500

    Passed in the name to generatedModel and used it in the registry

--- app/utils/generated-model.js
+++ app/utils/generated-model.js
@@ -1,11 +1,11 @@
 import config from '../config/environment';
 
-export default function generatedModel() {
+export default function generatedModel(name) {
   var classifiedName = Ember.String.classify(config.modulePrefix);
   if (Ember.testing) {
     // Can't easily see a way to plug into the window[classfiedName]
     return DS.Model;
   } else {
-    return window[classifiedName].__container__.registry["model:#{name}"] || DS.Model;
+    return window[classifiedName].__container__.registry["model:"+name] || DS.Model;
   }
 }
