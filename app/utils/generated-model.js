564ae0a717135fd81afa4e3e81c8201332134e4b
Nathan Palmer <nathan@nathanpalmer.com>
2015-02-24 08:12:35 -0500

    Loading the model as long as it's exists deep in the chain

--- app/utils/generated-model.js
+++ app/utils/generated-model.js
@@ -2,10 +2,12 @@ import config from '../config/environment';
 
 export default function generatedModel(name) {
   var classifiedName = Ember.String.classify(config.modulePrefix);
-  if (Ember.testing) {
-    // Can't easily see a way to plug into the window[classfiedName]
-    return DS.Model;
-  } else {
+  if (window[classifiedName] &&
+     window[classifiedName].__container__ &&
+     window[classifiedName].__container__.registry &&
+     window[classifiedName].__container__.registry["model:"+name]) {
     return window[classifiedName].__container__.registry["model:"+name] || DS.Model;
+  } else {
+    return DS.Model;
   }
 }
